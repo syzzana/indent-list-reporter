7e763b6a168021780417cf986b5ee8092a54b2af
syzzana <syzanabicaj@gmail.com>
2023-11-26 12:00:32 +0100

    feat: try fullyParallel false

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "tests",
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [["./src/indent-list-reporter.ts", {
     ignoreColors: false,

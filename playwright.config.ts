16229ab90935be7654c28afff65ae53c61f37d6c
syzzana <syzanabicaj@gmail.com>
2023-11-26 16:41:26 +0100

    fix: jest config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "playwright-tests",
+  testMatch: "playwright-tests/?(*.)+(spec).[tj]s?(x)",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 0 : 0,

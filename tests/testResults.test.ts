92d09fba82bef1fc7c6edde9e5988399d3f7776a
syzzana <syzanabicaj@gmail.com>
2023-11-28 22:33:58 +0100

    fix: add test for getting reporter options

--- tests/testResults.test.ts
+++ tests/testResults.test.ts
@@ -5,17 +5,29 @@ test('verify reporter is indent-list-reporter', () => {
     expect(reporter).toStrictEqual([true, 0]);
 });
 
+test('verify reporter is indent-list-reporter locally', () => {
+    const reporter = isIndentedListReporter([["./src/indent-list-reporter.ts"]])
+    expect(reporter).toStrictEqual([true, 0]);
+});
+
+test('verify we get repoter options for indent-list-reporter locally', () => {
+    const reporterOptions = getReporterOptions([["./src/indent-list-reporter.ts", {baseColors: {specFileNameColor: "red"}}]])
+    expect(reporterOptions).toStrictEqual({baseColors: {specFileNameColor: "red"}});
+});
 test('verify we get repoter options for indent-list-reporter', () => {
-   const reporter = isIndentedListReporter([["indent-list-reporter", {baseColors: {specFileNameColor: "red"}}]])
    const reporterOptions = getReporterOptions([["indent-list-reporter", {baseColors: {specFileNameColor: "red"}}]])
    expect(reporterOptions).toStrictEqual({baseColors: {specFileNameColor: "red"}});
 });
 
-test('verify we get do not repoter options indent-list-reporter', () => {
-    const reporterOptions = getReporterOptions([["./src/indent-list-reporter", {baseColors: {specFileNameColor: "red"}}]])
+test('verify we get undefied reporter options, when we do not have any', () => {
+    const reporterOptions = getReporterOptions([["indent-list-reporter"]])
     expect(reporterOptions).toBeUndefined();
 });
 
+test('verify we do not get repoter options for reporter who is not indent-list-reporter', () => {
+    const reporterOptions = getReporterOptions([["i-am-not-your-reporter", {randomObj: {attributes: "red"}}]])
+    expect(reporterOptions).toBeUndefined();
+});
 
 test('verify reporter is indent-list-reporter with more then one reporter', () => {
     const reporter = isIndentedListReporter([["line"],["indent-list-reporter"], ["html"]])

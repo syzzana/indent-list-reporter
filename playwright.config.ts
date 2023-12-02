e36ad2b79bbac8b7102181fdcc07a3b1de7df14a
syzzana <syzanabicaj@gmail.com>
2023-12-03 00:00:13 +0100

    test: with 0 retries in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     testMatch: "playwright-tests/**/*",
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 3,
+    retries: process.env.CI ? 0 : 3,
     workers: process.env.CI ? 1 : undefined,
     reporter: [["./src/indent-list-reporter.ts"]],
     use: {

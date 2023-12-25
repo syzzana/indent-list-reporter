80272bd30cbb81c6d82bb4bddffccf433dc14dd9
syzzana <syzanabicaj@gmail.com>
2023-12-25 16:16:20 +0100

    feat: move playwright test files to playwright folder

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 0 : 3,
     workers: process.env.CI ? 1 : undefined,
-    reporter: [["./src/indent-list-reporter.ts", {
+    reporter: [["../src/indent-list-reporter.ts", {
         ignoreColors: false,
         baseColors: {
             specFileNameColor: "magenta",

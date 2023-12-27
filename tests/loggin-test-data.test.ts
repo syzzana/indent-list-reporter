ac8ec98e3361fe22d2b672e2ecede043fef95fb3
syzzana <syzanabicaj@gmail.com>
2023-12-27 22:00:01 +0100

    fix: add placeholder test

--- tests/loggin-test-data.test.ts
+++ tests/loggin-test-data.test.ts
@@ -1,1 +1,7 @@
 //TODO add tests to verify colors are actually changed
+
+
+test("verify we get reporter options for indent-list-reporter locally", () => {
+    const actual = "200" 
+    expect(actual).toBe("200");
+});

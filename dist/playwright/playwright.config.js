75e10517c16409f0028b639150870e4ed3e5b0ee
syzzana <150734262+syzzana@users.noreply.github.com>
2023-12-27 14:49:32 +0100

    Fix/update version (#43)

---
+++ dist/playwright/playwright.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "@playwright/test";
+export default defineConfig({
+    testDir: "playwright-tests",
+    testMatch: "playwright-tests/**/*",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 0 : 3,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [["../src/indent-list-reporter.ts", {
+                ignoreColors: false,
+                baseColors: {
+                    specFileNameColor: "magenta",
+                    suiteDescriptionColor: "magenta",
+                    testCaseTitleColor: "blue",
+                },
+            }], ['json', { outputFile: 'test-results.json' }]],
+    use: {
+        ignoreHTTPSErrors: true,
+        trace: "off",
+        colorScheme: "dark",
+    },
+});

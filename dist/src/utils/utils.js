e54c6ac3dbd398fd4b1c6fcf1c27fcb7a7d74c0b
syzzana <syzanabicaj@gmail.com>
2024-03-01 23:45:49 +0100

    fix: ignore dist files

--- dist/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import os from "node:os";
-export const isWindows = os.platform().startsWith("win");
-export const adaptFilePathImportForWindows = (filePath) => {
-    let newFilePath = '';
-    if (isWindows) {
-        newFilePath = filePath.replace("/", '\\');
-    }
-    return `file:///${newFilePath}`;
-};
